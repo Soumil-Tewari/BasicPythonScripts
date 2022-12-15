aa24693a6d6d427755a6b403fc7fdf4f79f7651c
Soumil Tewari <75936954+Soumil-Tewari@users.noreply.github.com>
2022-12-15 08:37:58 +0530

    Create chessboard.py

---
+++ chessboard.py
@@ -0,0 +1,12 @@
+s=input("")
+if "a" in s or "c" in s or "e" in s or "g" in s:
+    if "1" in s or "3" in s or "5" in s or "7" in s:
+        print("Black")
+    else:
+        print("White")
+
+else:
+    if "1" in s or "3" in s or "5" in s or "7" in s:
+        print("White")
+    else:
+        print("Black")
